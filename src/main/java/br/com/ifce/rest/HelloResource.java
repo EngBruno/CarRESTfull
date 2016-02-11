92fb29344b28d027639cccff6ac71334af1e880a
Bruno Roberto <roberto.bruno007@gmail.com>
2016-02-11 20:52:31 -0300

    Setting up the project to use the API Jersey.

--- src/main/java/br/com/ifce/rest/HelloResource.java
+++ src/main/java/br/com/ifce/rest/HelloResource.java
@@ -1,5 +1,12 @@
 package br.com.ifce.rest;
 
-public class HelloResource {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
+@Path("/hello")
+public class HelloResource {
+	@GET
+	public String get(){
+		return "HTTP GET";
+	}
 }
