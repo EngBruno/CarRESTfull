b151b0e1087e8f61c8354073d993e901f37b0373
Bruno Roberto <roberto.bruno007@gmail.com>
2016-02-12 08:01:35 -0300

    Finishing the Hello Resource class, implementing the functions of the Web Service

--- src/main/java/br/com/ifce/rest/HelloResource.java
+++ src/main/java/br/com/ifce/rest/HelloResource.java
@@ -1,6 +1,9 @@
 package br.com.ifce.rest;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 
 @Path("/hello")
@@ -8,5 +11,20 @@ public class HelloResource {
 	@GET
 	public String get(){
 		return "HTTP GET";
+	}
+	
+	@POST
+	public String post(){
+		return "HTTP POS";
+	}
+	
+	@PUT
+	public String put(){
+		return "HTTP PUT";
+	}
+	
+	@DELETE
+	public String delete(){
+		return "HTTP DELETE";
 	}
 }
