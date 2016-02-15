35a90f49c21b0f5a3ee47cf9024f2441d60d5311
Bruno Roberto <roberto.bruno007@gmail.com>
2016-02-15 14:42:15 -0300

    Creating CarResource class that implements the web service.

--- src/main/java/br/com/ifce/rest/HelloResource.java
+++ src/main/java/br/com/ifce/rest/HelloResource.java
@@ -1,30 +1,42 @@
 package br.com.ifce.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.ifce.domain.Response;
 
 @Path("/hello")
 public class HelloResource {
 	@GET
-	public String get(){
-		return "HTTP GET";
+	@Consumes(MediaType.TEXT_HTML)
+	@Produces(MediaType.TEXT_HTML+";charset=utf-8")
+	public String helloHTML(){
+		return "<br> Ola mundo HTML!</br>";
 	}
 	
-	@POST
-	public String post(){
-		return "HTTP POS";
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloTextPlain(){
+		return "Ola mundo text";
 	}
 	
-	@PUT
-	public String put(){
-		return "HTTP PUT";
+	@GET
+	@Consumes({MediaType.APPLICATION_XML,MediaType.TEXT_XML})
+	@Produces({MediaType.APPLICATION_XML,MediaType.TEXT_XML})
+	public String helloXML(){
+		return "Ola mundo XML!";
 	}
 	
-	@DELETE
-	public String delete(){
-		return "HTTP DELETE";
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String helloJSON(){
+		return "Ola mundo do JSON!";
 	}
 }
--- src/main/java/br/com/ifce/rest/MyApplication.java
+++ src/main/java/br/com/ifce/rest/MyApplication.java
@@ -1,20 +1,20 @@
 package br.com.ifce.rest;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
+
 import javax.ws.rs.core.Application;
-import org.glassfish.jersey.jettison.JettisonFeature;
 
 public class MyApplication extends Application {
-	@Override
-	public Set<Object>getSingletons(){
-		Set<Object> singletons = new HashSet<>();
-		singletons.add(new JettisonFeature());
-		return singletons;
-	}
 	
+//	@Override
+//	public Set<Object>getSingletons(){
+//		Set<Object> singletons = new HashSet<>();
+//		singletons.add(new JettisonFeature());
+//		return singletons;
+//	}
+	
+	@Override
 	public Map<String,Object> getProperties(){
 		Map<String,Object> properties = new HashMap<>();
 		properties.put("jersey.config.server.provider.packages", "br.com.ifce");
